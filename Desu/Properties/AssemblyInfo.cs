cff1d17db5261d80bb4a078be463c488c68c5893
wacton <waacton@gmail.com>
2017-02-25 09:27:40 +0000

    Updated assembly description

--- Desu/Properties/AssemblyInfo.cs
+++ Desu/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Wacton.Desu")]
-[assembly: AssemblyDescription("Japanese and kanji dictionary (.NET wrapper for JMdict + KANJIDIC2 + KRADFILE + RADKFILE)")]
+[assembly: AssemblyDescription("Japanese and kanji dictionary (.NET wrapper for JMdict, KANJIDIC2, KRADFILE, RADKFILE, KanjiVG)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Wacton")]
 [assembly: AssemblyProduct("Wacton.Desu")]
