64f6a8fa4aed34c30276d2ceb1c765f3c2f2d2ed
wacton <waacton@gmail.com>
2017-07-12 15:16:16 +0100

    Assembly info update

--- Desu/Properties/AssemblyInfo.cs
+++ Desu/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Wacton.Desu")]
-[assembly: AssemblyDescription("Japanese and kanji dictionary .NET library built upon JMdict, KANJIDIC, KRADFILE/RADKFILE, and KanjiVG")]
+[assembly: AssemblyDescription("Japanese and kanji dictionary .NET library built upon JMdict, JMnedict, KANJIDIC, KRADFILE/RADKFILE, and KanjiVG")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Wacton")]
 [assembly: AssemblyProduct("Wacton.Desu")]
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.*")]
+[assembly: AssemblyVersion("3.1.0.*")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
